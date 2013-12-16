d7647cd34511ca6fa1dad30e58c393402f4e992e
Simon Whitaker <sw@netcetera.org>
2013-12-16 23:12:50 +0000

    Add more comments

--- Angry UIKit/SWViewController.m
+++ Angry UIKit/SWViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, strong) NSArray *originalObstacleViewCenters;
 @end
 
+// An enumeration defining the various stages of the demo. This just made it easier for me to switch between each stage of the demo as I took screencasts.
 typedef NS_ENUM(NSUInteger, SWDemoStage) {
     SWDemoStageGravity,
     SWDemoStageGravityWithBoundary,
@@ -24,9 +25,13 @@ typedef NS_ENUM(NSUInteger, SWDemoStage) {
 
 @implementation SWViewController
 
+// The current stage, i.e. the one I want to use when I run the app
 static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWithImages;
 
 - (void)viewDidLoad {
+    // I'll do some additional setup here, depending on which stage I'm running.
+    
+    // First position the dynamic view (that's the blue one - terrible name, I know!)
     if (demoStage >= SWDemoStageGravityWithBoundaryObstaclesAndPush) {
         self.dynamicView.center = CGPointMake(self.dynamicView.frame.size.width / 2, self.dynamicView.frame.size.height / 2);
     }
@@ -34,6 +39,7 @@ static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWit
         self.dynamicView.center = CGPointMake(self.view.center.x, 150);
     }
     
+    // Next position the obstacle (red and yellow) views, remove and that aren't in use in this stage
     if (demoStage == SWDemoStageGravityWithBoundaryAndObstacles) {
         NSArray *obstaclesToDelete = [self.obstacleViews filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"tag > 0"]];
         self.obstacleViews = [self.obstacleViews filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"tag == 0"]];
@@ -48,6 +54,7 @@ static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWit
         self.obstacleViews = nil;
     }
     
+    // Remove all subviews (images) from the views, unless we're running the final stage
     if (demoStage < SWDemoStageGravityWithBoundaryObstaclesAndPushWithImages) {
         for (UIView *view in [self.obstacleViews arrayByAddingObject:self.dynamicView]) {
             [view.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
@@ -68,21 +75,21 @@ static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWit
         // Initialize a dynamic animator
         self.dynamicAnimator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
         
-        // Get an array of all the views we want to add to stuff like the gravity behavior
+        // Get an array of all the views we want to add to gravity and collision behavior
         NSArray *allViews = [[NSArray arrayWithArray:self.obstacleViews] arrayByAddingObject:self.dynamicView];
 
         // Add the gravity behaviour
         UIGravityBehavior *gravityBehavior = [[UIGravityBehavior alloc] initWithItems:allViews];
         [self.dynamicAnimator addBehavior:gravityBehavior];
         
-        // Add boundaries
+        // Add the collision behaviour
         if (demoStage >= SWDemoStageGravityWithBoundary) {
             UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:allViews];
             collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
             [self.dynamicAnimator addBehavior:collisionBehavior];
         }
         else {
-            // If we have no boundaries, use the gravity behavior's activity block to detect when we go off screen and reset the view
+            // If we have no boundaries, we'll use the gravity behavior's activity block to detect when we go off screen (and hence should reset the view)
             gravityBehavior.action = ^{
                 if (!CGRectIntersectsRect(self.view.frame, self.dynamicView.frame)) {
                     [self SW_reset];
@@ -90,14 +97,15 @@ static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWit
             };
         }
         
-        // Give the main view a kick
+        // Give the dynamic (blue) view a kick, if needed
         if (demoStage >= SWDemoStageGravityWithBoundaryObstaclesAndPush) {
             UIPushBehavior *pushBehavior = [[UIPushBehavior alloc] initWithItems:@[self.dynamicView] mode:UIPushBehaviorModeInstantaneous];
             pushBehavior.magnitude = 15.0;
             pushBehavior.angle = -0.4;
             [self.dynamicAnimator addBehavior:pushBehavior];
         }
-        
+
+        // Set the delegate so that we can reset the view when dynamicAnimatorDidPause: gets called
         self.dynamicAnimator.delegate = self;
     }
 }
@@ -111,6 +119,8 @@ static SWDemoStage demoStage = SWDemoStageGravityWithBoundaryObstaclesAndPushWit
     double delayInSeconds = 1.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        
+        // Move all views back to their starting positions
         self.dynamicView.center = self.originalDynamicViewCenter;
         [self.obstacleViews enumerateObjectsUsingBlock:^(UIView *obstacleView, NSUInteger idx, BOOL *stop) {
             obstacleView.center = [self.originalObstacleViewCenters[idx] CGPointValue];
